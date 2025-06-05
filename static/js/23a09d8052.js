3694661b9a74bd0ebcdf404b58338c4b92a20835
baic <303394539@qq.com>
2025-06-05 10:35:28 +0800

    Updates

---
+++ static/js/23a09d8052.js
@@ -0,0 +1,12 @@
+"use strict";(self.webpackChunkyolk_docs=self.webpackChunkyolk_docs||[]).push([[569],{31149:function(o,n,_){_.r(n);var t=_(90896),a=_(97818),m=_(27632),c=_(62072),b=_(98463),s=_(80204),E=_(73781),x=_(79289),i=_(48531),d=_(1900),l=_(55826),r=_(18567),e=_(70813);function u(){return(0,e.jsx)(i.dY,{children:(0,e.jsx)(l.Suspense,{fallback:(0,e.jsx)(d.Z,{}),children:(0,e.jsx)(e.Fragment,{children:(0,e.jsxs)("div",{className:"markdown",children:[(0,e.jsxs)("h3",{id:"config",children:[(0,e.jsx)("a",{"aria-hidden":"true",tabIndex:"-1",href:"#config",children:(0,e.jsx)("span",{className:"icon icon-link"})}),"config"]}),(0,e.jsxs)("h5",{id:"\u5185\u7F6E-taro-\u914D\u7F6E\u7EE7\u627F\u4E8E-taro-\u7684-config",children:[(0,e.jsx)("a",{"aria-hidden":"true",tabIndex:"-1",href:"#\u5185\u7F6E-taro-\u914D\u7F6E\u7EE7\u627F\u4E8E-taro-\u7684-config",children:(0,e.jsx)("span",{className:"icon icon-link"})}),"\u5185\u7F6E taro \u914D\u7F6E\uFF0C\u7EE7\u627F\u4E8E taro \u7684 config"]}),(0,e.jsx)(s.Z,{lang:"tsx",children:r.texts[0].value})]})})})})}n.default=u},18567:function(o,n,_){_.r(n),_.d(n,{texts:function(){return t}});const t=[{value:`// config/index.ts
+import { config } from '@baic/yolk-taro';
+
+export default merge => {
+  return merge(
+    {},
+    config(merge, {
+      // \u81EA\u5B9A\u4E49Taro\u7684Config
+    }),
+  );
+};
+`,paraId:0,tocIndex:1}]}}]);
