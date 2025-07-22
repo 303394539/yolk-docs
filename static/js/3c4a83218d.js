17056f14c37510b6a864867f4e02f7a46aa7c9c5
baic <303394539@qq.com>
2025-07-22 15:49:29 +0800

    Updates

---
+++ static/js/3c4a83218d.js
@@ -0,0 +1,8 @@
+"use strict";(self.webpackChunkyolk_docs=self.webpackChunkyolk_docs||[]).push([[697],{7279:function(u,s,_){_.r(s);var t=_(37256),m=_(55291),c=_(28083),r=_(53454),x=_(56079),n=_(13190),h=_(35307),p=_(88075),o=_(46118),a=_(82312),i=_(55826),d=_(36470),e=_(70813);function l(){return(0,e.jsx)(o.dY,{children:(0,e.jsx)(i.Suspense,{fallback:(0,e.jsx)(a.Z,{}),children:(0,e.jsx)(e.Fragment,{children:(0,e.jsxs)("div",{className:"markdown",children:[(0,e.jsxs)("h3",{id:"wechat",children:[(0,e.jsx)("a",{"aria-hidden":"true",tabIndex:"-1",href:"#wechat",children:(0,e.jsx)("span",{className:"icon icon-link"})}),"Wechat"]}),(0,e.jsxs)("h5",{id:"\u5FAE\u4FE1\u516C\u4F17\u53F7-sdk-\u5E93\u5305\u542B-sdk-\u6240\u6709\u65B9\u6CD5\u548C\u6269\u5C55\u65B9\u6CD5",children:[(0,e.jsx)("a",{"aria-hidden":"true",tabIndex:"-1",href:"#\u5FAE\u4FE1\u516C\u4F17\u53F7-sdk-\u5E93\u5305\u542B-sdk-\u6240\u6709\u65B9\u6CD5\u548C\u6269\u5C55\u65B9\u6CD5",children:(0,e.jsx)("span",{className:"icon icon-link"})}),"\u5FAE\u4FE1\u516C\u4F17\u53F7 SDK \u5E93\uFF0C\u5305\u542B sdk \u6240\u6709\u65B9\u6CD5\u548C\u6269\u5C55\u65B9\u6CD5"]}),(0,e.jsx)(n.Z,{lang:"tsx",children:d.texts[0].value}),(0,e.jsx)(n.Z,{lang:"tsx",children:d.texts[1].value})]})})})})}s.default=l},36470:function(u,s,_){_.r(s),_.d(s,{texts:function(){return t}});const t=[{value:`import { Wechat } from '@baic/yolk-offiaccount';
+
+Wechat.closeWindow(); // \u5173\u95ED\u7A97\u53E3
+`,paraId:0,tocIndex:1},{value:`// \u63A5\u5408yolk-umi \u53EF\u901A\u8FC7umi\u76F4\u63A5\u4F7F\u7528
+import { Wechat } from 'umi';
+
+Wechat.closeWindow(); // \u5173\u95ED\u7A97\u53E3
+`,paraId:1,tocIndex:1}]}}]);
